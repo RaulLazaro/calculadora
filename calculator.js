36f507e34fff455899d609091ab98cb941d8500c
Raul Lazaro <raullazaro94@gmail.com>
2019-11-12 19:25:29 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,69 +1,41 @@
-const screen = document.getElementById('calculator__screen');
-const current = document.getElementById('screen__current');
-const result = document.getElementById('screen__result')
-const keys = document.getElementById('calculator__keys')
+const screen = document.getElementById("calculator__screen");
+const last = document.getElementById("screen__last");
+const current = document.getElementById("screen__current");
+const result = document.getElementById("screen__result");
+const keys = document.getElementById("calculator__keys");
 
-current.textContent = '0'
-
-const calculator = () => {
-  if (!keys) return
-  keys.addEventListener('click', e => {
-    const t = e.target,
-      d = t.dataset
-    if (d.number) writeScreen(d.number)
-    if (d.math) getMath(t, d.math)
-    if (d.operation) getOperation(d.operation)
-    screen.scrollTop = screen.scrollHeight
-  })
-}
-
-const writeScreen = number => {
-  current.textContent === '0' ?
-    current.textContent = number :
-    current.textContent += number
-}
-
-const getMath = (symbol, math) => {
-  if (Number(current.textContent[current.textContent.length - 1]))
-    current.textContent += '\n' + symbol.textContent + ' '
+function pulse(text) {
+  if (current.textContent =="0")
+    current.textContent = text;
   else
-    current.textContent = current.textContent.substring(0, current.textContent.length - 2) + symbol.textContent + ' '
+    current.textContent += text;
+  result.textContent = "= " + eval(current.textContent);
+  result.style.display = "block";
+  screen.scrollTop = screen.scrollHeight
 }
 
-const getOperation = operation => {
-  switch (operation) {
-    case 'clear':
-      current.textContent = '0'
-      break;
-    case 'erase':
-      current.textContent[current.textContent.length - 1] === ' ' ?
-        current.textContent = current.textContent.substring(0, current.textContent.length - 3) :
-        current.textContent = current.textContent.substring(0, current.textContent.length - 1)
-      break;
-    case 'percentage':
-      var position = current.textContent.lastIndexOf(' ')
-      if (position) {
-        var number = Number(current.textContent.substring(position, current.textContent.length))
-        number = number/100
-        current.textContent = current.textContent.substring(0, position + 1,) + number
-      }
-      else
-       {
-        var number = Number(current.textContent)
-        number = number/100
-        current.textContent = number
-      }
-      break;
-    case 'equal':
-      current.textContent = result.textContent
-      break;
-    default:
+function clearScreen() {
+  current.textContent = "0";
+  result.textContent = "";
+  result.style.display = "none";
+}
 
+function erase() {
+  if (current.textContent != "0" && current.textContent.length == 1){
+    current.textContent = "0";
+    result.textContent = "";
+    result.style.display = "none";
+  }
+  else {
+    current.textContent = current.textContent.slice(0, -1);
+    result.textContent = "= " + eval(current.textContent);
   }
 }
 
-const getResult = () => {
-
+function equal() {
+  last.textContent += current.textContent + " " + result.textContent + "\r\n";
+  current.textContent = result.textContent.slice(2);
+  result.textContent = "";
+  result.style.display = "none";
+  screen.scrollTop = screen.scrollHeight
 }
-
-calculator()
